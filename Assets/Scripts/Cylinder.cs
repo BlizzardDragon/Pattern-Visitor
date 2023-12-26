9a7d22020781bc927c35ade98d72a9c233fdd34b
BlizzardDragon <blizzarddragon@list.ru>
2023-12-26 19:20:14 +0300

    added PlayJump to Cylinder

--- Assets/Scripts/Cylinder.cs
+++ Assets/Scripts/Cylinder.cs
@@ -1,6 +1,14 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class Cylinder : MonoBehaviour, ITriggerVisitable
 {
+    [SerializeField] private Transform _view;
+    private Vector3 _startpLocalPosition;
+    
+    
+    private void Start() => _startpLocalPosition = _view.localPosition;
+    
     public void Accept(ITriggerVisitor visitor) => visitor.Visit(this);
+    public void PlayJump() => _view.DOLocalJump(_startpLocalPosition, 4, 1, 0.5f);
 }
--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -10,9 +10,23 @@ public class Trigger : MonoBehaviour, ITriggerVisitor
         }
     }
 
-    public void Visit(Cube cube) => Print(cube);
-    public void Visit(Sphere sphere) => Print(sphere);
-    public void Visit(Cylinder cylinder) => Print(cylinder);
+    public void Visit(Cube cube)
+    {
+        cube.PlayRotation();
+        Print(cube);
+    }
+
+    public void Visit(Sphere sphere)
+    {
+        sphere.PlayPunchScale();
+        Print(sphere);
+    }
+
+    public void Visit(Cylinder cylinder)
+    {
+        cylinder.PlayJump();
+        Print(cylinder);
+    }
 
     private void Print(MonoBehaviour mono) => print($"Visited {mono.name}!");
 }
