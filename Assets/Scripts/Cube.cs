5959f18dd0c480ded5cba0940280087d09fcda51
BlizzardDragon <blizzarddragon@list.ru>
2023-12-26 19:06:59 +0300

    added cube animation

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -1,6 +1,11 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class Cube : MonoBehaviour, ITriggerVisitable
 {
+    [SerializeField] private Transform _view;
+
+
     public void Accept(ITriggerVisitor visitor) => visitor.Visit(this);
+    public void PlayRotation() => _view.DOLocalRotate(Vector3.up * 360, 1f, RotateMode.FastBeyond360);
 }
