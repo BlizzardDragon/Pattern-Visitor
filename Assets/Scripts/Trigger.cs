457896529fd23ba13d870f6223f79acc521a7f38
BlizzardDragon <blizzarddragon@list.ru>
2023-12-26 18:31:35 +0300

    added Trigger

--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class Trigger : MonoBehaviour, ITriggerVisitor
 {
-
     private void OnTriggerEnter(Collider other)
     {
-
+        if (other.TryGetComponent(out ITriggerVisitable visitable))
+        {
+            visitable.Accept(this);
+        }
     }
 
     public void Visit(Cube cube) => Print(cube);
     public void Visit(Sphere sphere) => Print(sphere);
     public void Visit(Cylinder cylinder) => Print(cylinder);
 
-    private void Print(object type) => print($"Visited {nameof(type)}!");
+    private void Print(MonoBehaviour mono) => print($"Visited {mono.name}!");
 }
