7dd87f572d97ceb280b89c20d46ada05743234fd
BlizzardDragon <blizzarddragon@list.ru>
2023-12-26 19:07:11 +0300

    added sphere animation

--- Assets/Scripts/Sphere.cs
+++ Assets/Scripts/Sphere.cs
@@ -1,6 +1,11 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class Sphere : MonoBehaviour, ITriggerVisitable
 {
+    [SerializeField] private Transform _view;
+
+    
     public void Accept(ITriggerVisitor visitor) => visitor.Visit(this);
+    public void PlayPunchScale() => _view.DOPunchScale(Vector3.one * 1.5f, 1f, 4, 0);
 }
