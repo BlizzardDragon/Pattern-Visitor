183c69004969b12fc1cb8b96ba7056365bb66548
BlizzardDragon <blizzarddragon@list.ru>
2024-01-12 18:47:05 +0300

    fixed PrintUtility

--- Assets/Scripts/PrintUtility.cs
+++ Assets/Scripts/PrintUtility.cs
@@ -5,6 +5,6 @@ public static class PrintUtility
     public static void Print(string name, Color color)
     {
         string formattedText = $"<color=#{ColorUtility.ToHtmlStringRGB(color)}>{name}</color>";
-        print($"Visited {formattedText}!");
+        Debug.Log($"Visited {formattedText}!");
     }
 }
